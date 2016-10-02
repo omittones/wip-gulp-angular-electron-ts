0645a05ea3c5c4e85f9d6a7b92ea3943fd8a4338
Augustin Juricic <ajuricic@neogov.net>
2016-10-02 17:45:55 +0200

    Move infrastructure setup to components.

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,6 @@ import 'chart.js';
 import 'angular-chart.js';
 import * as Components from './components';
 
-(function(angular: ng.IAngularStatic, baseModule: any) {
-    baseModule = baseModule.bind(angular);
-    angular.module = function(name, requires, config): ng.IModule {
-        let module = baseModule(name, requires, config);
-        module.componentClass = function(factory: ng.ComponentConstructor) {
-            var controller: any = factory;
-            if (factory.$inject && factory.$inject.length > 0) {
-                controller = factory.$inject.slice();
-                controller.push(factory);
-            }
-            app.component(factory.$name, {
-                templateUrl: factory.$templateUrl,
-                template: factory.$template,
-                bindings: factory.$bindings,
-                controller: controller,
-                transclude: factory.$transclude
-            });
-            return app;
-        }
-        return module;
-    };
-})(angular, angular.module);
-
 var app = angular.module('minerDashboard', ['chart.js']);
 
 app.componentClass(Components.MinerStatusComponent);
